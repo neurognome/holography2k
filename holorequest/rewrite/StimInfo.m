0b3803ce9f9fc1f4fdcceeaf0734d5006cb29bad
Kevin SIt <33428056+neurognome@users.noreply.github.com>
2023-11-08 17:37:38 -0800

    Update StimInfo.m

--- holorequest/rewrite/StimInfo.m
+++ holorequest/rewrite/StimInfo.m
@@ -4,22 +4,42 @@ classdef StimInfo < handle
 
         hz
         power
-        on_time
-        duration
+        total_stimulation_time
+        pulse_duration
     end
 
 
     methods
-        function obj = StimInfo(firing_order, hz, power, on_time, duration)
-        obj.firing_order = firing_order;
-        obj.hz = hz;
-        obj.power = power;
-        obj.on_time = on_time;
-        obj.duration = duration;     
+        function obj = StimInfo(firing_order, hz, power, total_stimulation_time, pulse_duration)
+            if numel(power) > 2
+                disp('Can''t do multiple powers rn')
+                return
+            end
+            obj.firing_order = firing_order;
+            % checks
+            obj.hz = obj.check_and_equalize(hz);
+            obj.power = power;
+            obj.total_stimulation_time = obj.check_and_equalize(total_stimulation_time);
+            obj.pulse_duration= obj.check_and_equalize(pulse_duration);  
+        end
+
+        function out = check_and_equalize(obj, to_check)
+            desired_length = numel(obj.firing_order);
+            if length(to_check) == 1
+                out = repmat(to_check, 1, desired_length);
+                return
+            end
+            
+            if length(to_check) ~= desired_length
+                error('Either give me something with 1 length or matching')
+            end
+
+            out = to_check;
+
         end
 
         function out = trial_length(obj)
-            out = sum(obj.duration); % sum of all durations is the trial length
+            out = sum(obj.total_stimulation_time); % sum of all durations is the trial length
         end
 
         function out = N(obj)
