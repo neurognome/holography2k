2aa96ee14fe0643bf50f0c58d8ea5c6dd3f54373
Kevin SIt <33428056+neurognome@users.noreply.github.com>
2024-07-22 16:48:00 -0700

    Update Sequence.m

--- roi_class/Sequence.m
+++ roi_class/Sequence.m
@@ -7,14 +7,21 @@ classdef Sequence < handle
         function obj = Sequence(patterns)
             obj.patterns = patterns;
         end
+
+        function equalize_patterns(obj)
+        % this is a bandaid fix because of lack of power modulation
+            warning('Pattern equalization enabled, modulating power with SLM.')
+            
+        end
         
-        function out = generate_sequence(obj)            
-            out = zeros(1, length(obj.patterns));
-            ct = 1;
-            for p = obj.patterns
-                out(ct) = p.id;
-                ct = ct + 1;
-            end
+        function out = ids(obj)            
+            out = [obj.patterns.id];
+        end
+
+        function out = average_de(obj)
+            % right now we can't really "flip" fast enough to change power
+            % that carefully... so let's just choose an average lol
+            out = sum([obj.patterns.diffraction_efficiency])/numel(obj.patterns);
         end
     end
 end
