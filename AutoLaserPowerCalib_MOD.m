28255a749c15811362c9baab1688e214d84f7284
Kevin SIt <33428056+neurognome@users.noreply.github.com>
2024-07-22 07:42:58 -0700

    added more laser control

--- AutoLaserPowerCalib_MOD.m
+++ AutoLaserPowerCalib_MOD.m
@@ -8,55 +8,49 @@ clc
 close all;
 
 % fill this in
-wavelength = 1100; % 900, 1100, 1030
+wavelength = 1030; % 870, 900, 1030
 used_khz = 250;
-aom = 0.30;
-gate = 'uni';%'uni'; % or none or normal?
+% used_khz = 50;
+% aom = 0.30;
+% gate = 'uni';%'uni'; % or none or normal?
 
 save_base = 'C:\Users\holos\Documents\power-calibrations\';
 
 %% start visa thing (older matlabs)
+% note: use instrhwinfo to find the correct dev
+addpath(genpath('C:\Users\holos\Documents\GitHub\holodaq'));
 
-tpm  = ThorlabsPowerMeter();
+instrreset()
+vinfo = instrhwinfo('visa','ni');
+v = eval(vinfo.ObjectConstructorName{1});
+fopen(v);
 
 %% Params
-nsamplesPM = 1000; % counts (at 1000 Hopen wavz I think), produces an average
-interStepPause = 0.5; % seconds (0.5 for the fast head)
-
-tpm.set_avg_samples(nsamplesPM);
-tpm.set_timeout(3+1.1*nsamplesPM*3/1000);
+nsamplesPM = 1000; % counts (at 1000 Hz I think), produces an average
+interStepPause = 5; % seconds (0.5 for the fast head, 5 for the thermal)
 
 %% initialize the thing
 
 dq = daq('ni');
 
-s = serialport("COM5", ...
-    9600,...
-    'ByteOrder', 'big-endian',...
-    'Parity', 'none',...
-    'StopBits', 1,...
-    'DataBits', 8);
-s.configureTerminator('CR/LF');
-
 switch wavelength
     case 900
         dq.addoutput('Dev1', 'port0/line5', 'Digital'); % 0/5: 920, 0/6: 1100
-        hwp = ELL14(SerialInterface(s), 1, 'hwp'); % 0: 900, 1:1100 % might need both? idk
-        tpm.set_wavelength(960);
-    case 1100
-        dq.addoutput('Dev1', 'port0/line4', 'Digital'); % 0/5: 920, 0/6: 1100
-        hwp = ELL14(SerialInterface(s), 2, 'hwp'); % 0: 900, 1:1100 % might need both? idk
-        tpm.set_wavelength(1100);
+        dq.addoutput('Dev1', 'ao0', 'Voltage');
+        max_v = 1.5;
+    case 870
+        dq.addoutput('Dev1', 'port0/line5', 'Digital'); % 0/5: 920, 0/6: 1100
+        dq.addoutput('Dev1', 'ao0', 'Voltage');
+        max_v = 1.5;
     case 1030
         dq.addoutput('Dev1', 'port0/line6', 'Digital');
-        hwp = ELL14(SerialInterface(s), 3, 'hwp');
-        tpm.set_wavelength(1030);
+        dq.addoutput('Dev1', 'ao1', 'Voltage');
+        max_v = 5;
 end
 disp('Devices connected.')
 
 %% 
-hwp.set(0); % start at 0
-dq.write(0); % close shutter
+dq.write([0, 0]); % start at 0
 
 %%
 % disp('Do this calibration with the z-block in place')
@@ -64,24 +58,27 @@ dq.write(0); % close shutter
 % disp('Put Laser Gate on Bypass') %added 3/18/21
 
 %% initial search for low and high points
-initial_search_queries = linspace(0, 120, 60*2); % 0 to 70
+initial_search_queries = linspace(0, max_v, 60); % 0 to 70
 
 initial_search_values = zeros(size(initial_search_queries));
 
 % start
-dq.write(1);
+dq.write([1, 0]);
 for ii = 1:numel(initial_search_queries)
-    hwp.set(initial_search_queries(ii)); % move to deg
+    dq.write([1, initial_search_queries(ii)])
     pause(interStepPause);
 
-    val = tpm.read()   * 1000; % in mW
+    fprintf(v, ['sense:average:count ', num2str(nsamplesPM)]);
+    set(v, 'timeout', 3+1.1*nsamplesPM*3/1000)
+    ret = query(v, 'read?');
+    val = str2double(ret)*1000;
     if val < 0
         val = 0;
     end
     initial_search_values(ii) = val;
     disp(['Deg: ' num2str(initial_search_queries(ii)) ' Power (mW):  ' num2str(val)])
 end
-dq.write(0);
+dq.write([0, 0]);
 
 %% find hi-low
 [max_pwr, max_idx] = max(initial_search_values(3:end-3)); %exclude ends
@@ -98,8 +95,6 @@ fprintf('Initial Search Report\n Max: %0.2fmW at %0.2fdeg \n Min: %0.2fmW and %0
 
 initial_search_values = initial_search_values/1000; % convert to W
 
-
-%% interp only
 %% information
 calib = struct();
 calib.max_deg = initial_search_queries(max_idx);
@@ -112,8 +107,7 @@ calib.powers = initial_search_values(segment);
 calib.khz = used_khz;
 
 %% save
-fn = fullfile(save_base, sprintf('%s_%dnm_%dkHz_%dAOM_%s_gate_calibration_temp.mat', datetime('now', 'Format', 'yyMMdd'), wavelength, used_khz, aom*100, gate));
+fn = fullfile(save_base, sprintf('%s_%dCnm_%dkHz_calibration_MOD.mat', datetime('now', 'Format', 'yyMMdd'), wavelength, used_khz));
 save(fn, 'calib')
-save(sprintf('active_%d.mat', wavelength), 'calib')
-
+fn2 = fullfile(save_base, sprintf('active_%daom.mat', wavelength));
 fprintf('Saved in %s\n', fn)
--- alignment/alignCodeDAQ2K.m
+++ alignment/alignCodeDAQ2K.m
@@ -3,12 +3,12 @@ function alignCodeDAQ2K
 fprintf('Starting daq...\r')
 
 fprintf('Loading defaults... ')
-setup = getDefaults();  
+%setup = getDefaults();  
 fprintf('OK.\n')
 
 fprintf('Making MATLAB NIDAQ object... ')
 dq = daq('ni');
-dq.Rate = setup.daqrate;
+dq.Rate = 20000;%setup.daqrate;
 fprintf('OK.\n')
 
 fprintf('Making SerialPort object... ')
@@ -23,23 +23,24 @@ fprintf('OK.\n')
 %%
 power_calibrations;
 %initalize contact
-fpc_900 = FiberPowerControl(Output(DAQOutput(dq, 'port0/line5'), 'Shutter 900'),...
+fpc_900 = LaserPowerControl(Output(DAQOutput(dq, 'port0/line5'), 'Shutter 900'),...
     ELL14(SerialInterface(s), 1, 'Power 900'),...
-     power_calibration.calibration_900, 25); % update these calibration paths as you get them...
+     power_calibration.calibration_900, 250); % update these calibration paths as you get them...
 
-fpc_1100 = FiberPowerControl(Output(DAQOutput(dq, 'port0/line4'), 'Shutter 1100'),...
+fpc_1100 = LaserPowerControl(Output(DAQOutput(dq, 'port0/line4'), 'Shutter 1100'),...
     ELL14(SerialInterface(s), 2, 'Power 1100'),...
-    power_calibration.calibration_1100, 25);
+    power_calibration.calibration_1100, 50);
 
-fpc_1030 = FiberPowerControl(Output(DAQOutput(dq, 'port0/line6'), 'Shutter 1030'),...
+fpc_1030 = LaserPowerControl(Output(DAQOutput(dq, 'port0/line6'), 'Shutter 1030'),...
     ELL14(SerialInterface(s), 3, 'Power 1030'),...
-    power_calibration.calibration_1030, 10);
+    power_calibration.calibration_1030H, 500);
 %%
 
 [HoloSocket]=msocketPrep();
 calibration_wavelength = msrecv(HoloSocket, 5); % long timeout
 
 fprintf('Calibrating %dnm.\n', calibration_wavelength)
+
 switch calibration_wavelength
     case 900
         pwr = fpc_900;
